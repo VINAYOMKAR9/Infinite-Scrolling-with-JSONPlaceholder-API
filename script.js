32ce6b25e841c8e327636340433acc06c94d924b
VINAYOMKAR9 <vinayomkar9@gmail.com>
2024-06-12 09:05:57 +0530

    Infinite is scrolling

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 let container = document.querySelector('#container')
 let url = 'https://jsonplaceholder.typicode.com/photos'
 let page = 1
-let flag = false;
+let flag = false; // when we scrooll at bottom its  invoke data before load to current data its a type of glitch to prevent this we using flag 
 getData(url,page)
+
+// Here we Fetching the data
 async function getData(url,page){
 try{
     let res =await fetch(`${url}?_page=${page}&_limit=10`)
     let data =await res.json();
     console.log(data);
-    displayData(data)
+
+    displayData(data) // invoke the  displayData function that display the data in webpage
 }
 catch(err){
     console.log('Error in code');
 }
 }
 
-function displayData(data){
+function displayData(data){  
+    //this help to show the data in webpage its called mapping 
     data.map((el,ind)=>{
         let card = document.createElement('div')
 
@@ -29,19 +33,23 @@ function displayData(data){
         container.append(card)
 
     })
-    flag = true 
+    flag = true  // after  load the whole page we change the flag to true  
 }
 
+
+
+// we provide addEventListener here when we scroll this fuction will run
 window.addEventListener('scroll',()=>{
-   let clientHeight= document.documentElement.clientHeight
-   let scrollHeight= document.documentElement.scrollHeight
-   let scrollTop= document.documentElement.scrollTop
+   let clientHeight= document.documentElement.clientHeight  // its the visible  height of data that see to user
+   let scrollHeight= document.documentElement.scrollHeight // its the total height of data
+   let scrollTop= document.documentElement.scrollTop // its the  height of data that not visible to user or remaining height
     console.log(clientHeight,scrollHeight,scrollTop);
 
-    if(Math.ceil(scrollHeight-clientHeight)<=Math.ceil(scrollTop)){
+    if(Math.ceil(scrollHeight-clientHeight)<=Math.ceil(scrollTop)){   // Its height calculation like (10 - 4) <= (6)  where 10 is total height , 4 is visible height and 6 is remaining height and in this equation we comapreing that...
+
         console.log('bottom');
         page++;
         getData(url,page)
-        flag = false
+        flag = false   // after we come to bottom , the flag will false 
     }
 })
